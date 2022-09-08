6711f8cfc981bcd912c920b50f5541b9a4729691
Seany-mibb <86206802+Seany-mibb@users.noreply.github.com>
2022-09-07 20:01:33 -0700

    Add files via upload

--- quiz_page.js
+++ quiz_page.js
@@ -1,6 +1,9 @@
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
 
+var respond = 0;
+
+var actual_answer = 0;
 player1_score = 0;
 player2_score = 0;
 document.getElementById("player1_name").innerHTML = player1_name + ": ";
@@ -26,47 +29,32 @@ function send()
     document.getElementById("number2").value = "";
 }
 
-question_turn = "player1";
-answer_turn = "player2";
+var question_turn = 'player1';
+var answer_turn = 'player2';
 
 function check()
 {
-    get_answer = document.getElementById("input_check_box").value;
-    answer = get_answer.toLowerCase();
-    if(answer == number1 * number2)
+    respond = document.getElementById("input_check_box").value;
+    respond = parseInt(respond)
+    actual_answer = parseInt(number1) * parseInt(number2);
+    if(respond === actual_answer)
     {
-        if(answer_turn == "player1")
+        if(answer_turn == 'player1')
         {
-            player1_score += 1;
-            document.getElementById("player1_score").innerHTML = player1_score;
+            player1_score++;
+            question_turn = 'player1';
+            answer_turn = 'player2';
+            document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+            document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
         }
-        else
+
+        if(answer_turn == 'player2')
         {
-            player2_score += 1;
-            document.getElementById("player2_score").innerHTML = player2_score;
+            player2_score++;
+            question_turn = 'player2';
+            answer_turn = 'player1';
+            document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+            document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
         }
     }
-
-    if(question_turn == "player1")
-    {
-        question_turn = "player2";
-        document.getElementById("player_question").innerHTML = "Question Turn:" + player2_name;
-    }
-    else
-    {
-        question_turn = "player1";
-        document.getElementById("player_question").innerHTML = "Question Turn:" + player1_name;
-    }
-
-    if(answer_turn == "player1")
-    {
-        answer_turn = "player2";
-        document.getElementById("player_answer").innerHTML = "Answer Turn:" + player2_name;
-    }
-    else
-    {
-        answer_turn = "player1";
-        document.getElementById("player_answer").innerHTML = "Answer Turn:" + player1_name
-    }
-    document.getElementById("output").innerHTML = "";
 }
